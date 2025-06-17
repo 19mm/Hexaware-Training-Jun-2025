e4295ef73af233dea2c531067cf224d84baad71a
Mayuresh Firodiya <92072778+19mm@users.noreply.github.com>
2025-06-17 16:51:40 +0530

    Add files via upload

---
+++ SpringBoot/com.hexaware.web1/src/main/java/com/example/demo/MController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@SuppressWarnings("unused")
+@Controller
+public class MController {
+	@RequestMapping("/home")
+	public String home(){
+		return "home";
+	}
+	@RequestMapping("/About")
+	public String About(){
+		return "About";
+	}
+}
+	
